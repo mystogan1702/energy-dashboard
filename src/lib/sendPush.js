8145c72cd003fe479abd772ef60eb93a06518a88
mystogan1702 <jadcabaddu1072@gmail.com>
2026-07-02 19:53:45 +0800

    push message

--- src/lib/sendPush.js
+++ src/lib/sendPush.js
@@ -1,5 +1,5 @@
 const ONE_SIGNAL_APP_ID = "45fad956-4485-4484-aacb-582da8a98b48";
-const ONE_SIGNAL_REST_KEY = "os_v2_app_ix5nsvseqvcijkwllaw2rkmljc7wlkb45bpe6of4mnu3pbpcswstrkmxq4het66k7ri6rb3kjbi2hq7fexkj4zucx3yy3jphkpopi3q";   // paste from step 2
+const ONE_SIGNAL_REST_KEY = import.meta.env.VITE_ONESIGNAL_REST_KEY;;   // paste from step 2
 
 export async function sendPushNotification(title, message, url) {
   try {
